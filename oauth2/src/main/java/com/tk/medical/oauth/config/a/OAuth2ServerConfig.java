e1f8d590d9b73c48a214a07ed9f41fbe73c59c53
lenovo <lizhen_init@163.com>
2020-03-19 17:30:30 +0800

    OAuth2的版本兼容性有问题

--- oauth2/src/main/java/com/tk/medical/oauth/config/a/OAuth2ServerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.tk.medical.oauth.config.a;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class OAuth2ServerConfig {
-    private static  final String DEMO_RESOURCE_ID = "text";
-
-}
