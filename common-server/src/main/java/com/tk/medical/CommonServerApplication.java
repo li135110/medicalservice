b2124d7ad62ada6a4bd998e1804af280d4aa5b01
lenovo <lizhen_init@163.com>
2020-03-25 17:30:41 +0800

    完成公用模块

--- common-server/src/main/java/com/tk/medical/CommonServerApplication.java
+++ common-server/src/main/java/com/tk/medical/CommonServerApplication.java
@@ -2,12 +2,16 @@ package com.tk.medical;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CommonServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommonServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CommonServerApplication.class, args);
+    }
 
 }
--- eureka-server1/src/main/java/com/tk/medical/EureakServer1Application.java
+++ eureka-server1/src/main/java/com/tk/medical/EureakServer1Application.java
@@ -3,9 +3,10 @@ package com.tk.medical;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 public class EureakServer1Application {
     public static void main(String[] args) {
